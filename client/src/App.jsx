372fa132f6531c908d96bc49d4023f0326461518
Ramya Prasanna <ramya.prasanna06@gmail.com>
2025-07-27 02:42:30 -0400

    played around with data

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <h1>DNA SEQUENCE ANALYZER</h1>
+      </header>
+      <div className='upload'>
+        <h2>UPLOAD A FILE</h2>
+      </div>
+      <div>
+        <h3>buttons</h3>
+      </div>
+      <div>
+        <h3>big box</h3>
+      </div>
+    </main>
+  )
+}
+
+export default App
